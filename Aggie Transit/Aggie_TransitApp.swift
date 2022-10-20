375ec7d1618ab70249605a4e3144273ec5a35a3d
N1v3x2 <n1v3x@tamu.edu>
2022-10-20 00:57:33 -0500

    Initial Commit

---
+++ Aggie Transit/Aggie_TransitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aggie_TransitApp.swift
+//  Aggie Transit
+//
+//  Created by Kevin Zhang on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Aggie_TransitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
