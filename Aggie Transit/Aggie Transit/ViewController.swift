d067fb4c31f73cc35f17e6180e805653be2a15b6
Soham Nagawanshi <ndsoham@gmail.com>
2022-11-04 09:38:27 -0500

    Created UIKit project

---
+++ Aggie Transit/Aggie Transit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aggie Transit
+//
+//  Created by Soham Nagawanshi on 11/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
