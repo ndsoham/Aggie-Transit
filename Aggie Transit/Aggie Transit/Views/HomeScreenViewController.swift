96165df62fda78ac6aaede35979dd827b536b1fa
Soham Nagawanshi <ndsoham@gmail.com>
2022-11-04 10:03:34 -0500

    Initialized needed view files
    Initialized home screen, home screen modal, settings screen, and notifications screen.

---
+++ Aggie Transit/Aggie Transit/Views/HomeScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeScreenViewController.swift
+//  Aggie Transit
+//
+//  Created by Soham Nagawanshi on 11/4/22.
+//
+
+import Foundation
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+    
+}
---
+++ Aggie Transit/Aggie Transit/Views/NotificationsScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationsScreenViewController.swift
+//  Aggie Transit
+//
+//  Created by Soham Nagawanshi on 11/4/22.
+//
+
+import Foundation
+import UIKit
+
+class NotificationsScreenViewController: UIViewController {
+    
+}
