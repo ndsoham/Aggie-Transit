f2224224e9c278812918f24f4588a90e509fbab4
Soham Nagawanshi <ndsoham@gmail.com>
2022-12-01 13:44:30 -0600

    implemented home screen menu view version 1
    need to implement page control

---
+++ Aggie Transit/Aggie Transit/Views/Animator.swift
@@ -0,0 +1,8 @@
+//
+//  Animator.swift
+//  Aggie Transit
+//
+//  Created by Soham Nagawanshi on 11/30/22.
+//
+
+import Foundation
---
+++ Aggie Transit/Aggie Transit/Views/HomeScreenMenuView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeScreenMenuView.swift
+//  Aggie Transit
+//
+//  Created by Soham Nagawanshi on 12/1/22.
+//
+
+import Foundation
