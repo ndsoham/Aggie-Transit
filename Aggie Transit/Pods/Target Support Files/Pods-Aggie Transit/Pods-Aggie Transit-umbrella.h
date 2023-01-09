2e9789c21962de689eb98b4949fffb1624c85b25
Soham Nagawanshi <ndsoham@gmail.com>
2023-01-08 21:40:28 -0600

    modified back end algorithm

---
+++ Aggie Transit/Pods/Target Support Files/Pods-Aggie Transit/Pods-Aggie Transit-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Aggie_TransitVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Aggie_TransitVersionString[];
+
